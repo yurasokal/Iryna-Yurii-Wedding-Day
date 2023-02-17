fc06c5b311fdd52be9e0216f3b72e89035b30431
Yurs Chabarenko <yurasokal>
2023-02-17 13:27:27 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Here are will be our IRYNA & YURII wedding day website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wait, bitte :)
-        </a>
       </header>
     </div>
   );
